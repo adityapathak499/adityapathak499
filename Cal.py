2d53d3406ffa4d40c62e78eefb18a2c220441687
adityapathak499 <100963681+adityapathak499@users.noreply.github.com>
2022-03-05 00:35:00 +0530

    Add files via upload

---
+++ Cal.py
@@ -0,0 +1,13 @@
+num1=int(input("Enter the first number "))
+num2=int(input("Enter the first number "))
+opr=input('Enter the opr')
+if opr=="+":
+    print(num1+num2)
+elif opr=="-":
+        print(num1-num2)
+elif opr=="*":
+            print(num1*num2)
+elif opr=="/":
+                print(num1/num2)
+else :
+  print("Invalid......")
